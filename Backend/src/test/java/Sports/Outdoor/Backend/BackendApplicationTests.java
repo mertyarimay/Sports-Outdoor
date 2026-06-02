0b030687ffc292288bd1aecb030bf3e025bd03c4
mertY44 <150911448+mertY44@users.noreply.github.com>
2026-06-02 17:29:25 +0300

    first commit

---
+++ Backend/src/test/java/Sports/Outdoor/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Sports.Outdoor.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
